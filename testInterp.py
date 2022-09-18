aab1d9f8d198d915e6f53f218df0917f4e03c01c
Hsinju Chen <hsinjuc2@illinois.edu>
2022-09-18 16:52:20 -0500

    MAINT: Clean up file

--- testInterp.py
+++ testInterp.py
@@ -16,67 +16,36 @@ def main():
     #r     = np.linspace( RE,   20.*RE, Nr )
     #theta = np.linspace( 0.,    np.pi, Nt )
     #phi   = np.linspace( 0., 2.*np.pi, Np )
-    x = np.linspace( -20.*RE, 20.*RE, Nx)
-    y = np.linspace( -20.*RE, 20.*RE, Ny)
-    z = np.linspace( -20.*RE, 20.*RE, Nz)
+    X = np.linspace( -20.*RE, 20.*RE, Nx)
+    Y = np.linspace( -20.*RE, 20.*RE, Ny)
+    Z = np.linspace( -20.*RE, 20.*RE, Nz)
     
-    Bmagc = np.empty((Nx,Ny,Nz))
-    Bxc   = np.empty((Nx,Ny,Nz))
-    Byc   = np.empty((Nx,Ny,Nz))
-    Bzc   = np.empty((Nx,Ny,Nz))
+    Bx,By,Bz,Bmag = bearth.getEarthDipole(X,Y,Z)
     
-    for i in range(0,Nx):
-        for j in range(0,Ny):
-            for k in range(0,Nz):
-                #if abs(x[i])<RE and abs(y[j])<RE and abs(z[k])<RE:
-                if abs(x[i]/RE)==0. and abs(y[j]/RE)==0. and abs(z[k]/RE)==0.:
-                    Bmagc[i,j,k] = np.nan
-                    Bxc[i,j,k]   = np.nan
-                    Byc[i,j,k]   = np.nan
-                    Bzc[i,j,k]   = np.nan
-                else:
-                    Bxc[i,j,k],Byc[i,j,k],Bzc[i,j,k] = bearth.dipoleEarth(x[i],y[j],z[k])
-                    Bmagc[i,j,k] = np.sqrt( Bxc[i,j,k]**2 + Byc[i,j,k]**2 + Bzc[i,j,k]**2 )
-    #xp = 2.5*RE
-    #yp = 2.2*RE
-    #zp = 3.2*RE
-    #print(particle.interp3Dalt(x,y,z,Bxc,Byc,Bzc,xp,yp,zp))
-    #print(bearth.dipoleEarth(xp,yp,zp))
-    
-    refine  = 2.
-    xrefine = np.linspace( -20.*RE, 20.*RE, int(refine*(Nx-1)+1))
-    yrefine = np.linspace( -20.*RE, 20.*RE, int(refine*(Ny-1)+1))
-    zrefine = np.linspace( -20.*RE, 20.*RE, int(refine*(Nz-1)+1))
+    refine  = 3.
+    Xrefine = np.linspace( -20.*RE, 20.*RE, int(refine*(Nx-1)+1))
+    Yrefine = np.linspace( -20.*RE, 20.*RE, int(refine*(Ny-1)+1))
+    Zrefine = np.linspace( -20.*RE, 20.*RE, int(refine*(Nz-1)+1))
     # interpolated b field array initialization
     Bxi     = np.zeros((int(refine*(Nx-1)+1), int(refine*(Ny-1)+1), int(refine*(Nz-1)+1)))
     Byi     = np.zeros((int(refine*(Nx-1)+1), int(refine*(Ny-1)+1), int(refine*(Nz-1)+1)))
     Bzi     = np.zeros((int(refine*(Nx-1)+1), int(refine*(Ny-1)+1), int(refine*(Nz-1)+1)))
-    #j     = 50
     for i in range(0,int(refine*(Nx-1)+1)):
         for j in range(0,int(refine*(Ny-1)+1)):
-            # xp = xrefine[i]
-            # zp = zrefine[k]
-            # Bxi[i,j,k],Byi[i,j,k],Bzi[i,j,k] = particle.interp2D(x,z,Bxc[:,10,:],Byc[:,10,:],Bzc[:,10,:],xp,zp)
             for k in range(0,int(refine*(Nz-1)+1)):
-                if abs(xrefine[i])<RE and abs(yrefine[j])<RE and abs(zrefine[k])<RE:
+                if abs(Xrefine[i])<RE and abs(Yrefine[j])<RE and abs(Zrefine[k])<RE:
                     Bxi[i,j,k] = np.nan
                     Byi[i,j,k] = np.nan
                     Bzi[i,j,k] = np.nan
                 else:
-                    if i%refine == 0 and j%refine == 0 and k%refine == 0:
-                        Bxi[i,j,k] = Bxc[int(i/refine),int(j/refine),int(k/refine)]
-                        Byi[i,j,k] = Byc[int(i/refine),int(j/refine),int(k/refine)]
-                        Bzi[i,j,k] = Bzc[int(i/refine),int(j/refine),int(k/refine)]
-                        #print('hi')
-                    else:
-                        xp = xrefine[i]
-                        yp = yrefine[j]
-                        zp = zrefine[k]
-                        Bxi[i,j,k],Byi[i,j,k],Bzi[i,j,k] = particle.interp3D(x,y,z,Bxc,Byc,Bzc,xp,yp,zp)
+                    xp = Xrefine[i]
+                    yp = Yrefine[j]
+                    zp = Zrefine[k]
+                    Bxi[i,j,k],Byi[i,j,k],Bzi[i,j,k] = particle.interp3D(X,Y,Z,Bx,By,Bz,xp,yp,zp)
     
     plt.figure(1)
-    xx,zz = np.meshgrid(x,z)
-    plt.contourf(np.transpose(xx)/RE,np.transpose(zz)/RE, Bxc[:,10,:])
+    XX,ZZ = np.meshgrid(X,Z)
+    plt.contourf(np.transpose(XX)/RE,np.transpose(ZZ)/RE, Bx[:,10,:])
     plt.xlabel('$X$ [$R_E$]')
     plt.ylabel('$Z$ [$R_E$]')
     plt.axis('equal')
@@ -84,8 +53,8 @@ def main():
     plt.title('Bx in XZ plane [T] - Earth magnetic field')
 
     plt.figure(2)
-    xxrefine,zzrefine = np.meshgrid(xrefine,zrefine)
-    plt.contourf(np.transpose(xxrefine)/RE,np.transpose(zzrefine)/RE, Bxi[:,int(10*refine),:])
+    XXrefine,ZZrefine = np.meshgrid(Xrefine,Zrefine)
+    plt.contourf(np.transpose(XXrefine)/RE,np.transpose(ZZrefine)/RE, Bxi[:,int(10*refine),:])
     plt.xlabel('$X$ [$R_E$]')
     plt.ylabel('$Z$ [$R_E$]')
     plt.axis('equal')
