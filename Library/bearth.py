f58ccce45669a424d8ee87c048519004a68f26ef
Hsinju Chen <hsinjuc2@illinois.edu>
2022-09-24 02:39:16 -0500

    DEV: Add field line tracing

--- Library/bearth.py
+++ Library/bearth.py
@@ -44,10 +44,8 @@ def dipoleEarthXY0(x,y):
     # reference: eq.1-3 https://ccmc.gsfc.nasa.gov/static/files/Dipole.pdf
     #M  = -8.e15 # dipole moment of Earth [Tm^3]
     r = np.sqrt(x**2+y**2)
-    Bx = 3.*M*x*z/r**5
-    By = 3.*M*y*z/r**5
-    #Bz = M*(3.*z*z-r*r)/r**5
-    return Bx,By
+    Bz = -M/r**3
+    return Bz
 
 def getEarthDipoleCSC(X,Y,Z):
     '''
@@ -119,3 +117,72 @@ def getEarthDipoleSph(R,Theta):
             else:
                 Br[i,j],Bt[i,j],Bp[i,j] = dipoleEarthSph(R[i],Theta[j])
     return Br,Bt,Bp
+
+# NOT WORKING
+def dipoleFieldline2DSph(r,theta,dr,rdtheta,nstep):
+    R = [r]
+    Theta = [theta]
+    for i in range(0,nstep):
+        Br,Bt,Bp = dipoleEarthSph(r,theta)
+        theta += Bt*rdtheta/abs(Br)
+        r     += Br*dr/abs(Br)
+        R      = np.append(R,r)
+        Theta  = np.append(Theta,theta)
+        print(r,theta)
+    return R,Theta
+
+def dipoleFieldline3D(x,y,z,dx,dy,dz,nstep):
+    X = [x]
+    Y = [y]
+    Z = [z]
+    for i in range(0,nstep):
+        Bx,By,Bz = dipoleEarth(x,y,z)
+        Bmag     = np.sqrt(Bx**2+By**2+Bz**2)
+        x       += dx*Bx/Bmag
+        y       += dy*By/Bmag
+        z       += dz*Bz/Bmag
+        X        = np.append(X,x)
+        Y        = np.append(Y,y)
+        Z        = np.append(Z,z)
+        print(x,y,z)
+    return X,Y,Z
+
+def dipoleFieldline2D(Lshell,MLT,dx,dz):
+    x = Lshell*RE
+    y = 0.
+    z = 0.
+    X = [x]
+    Z = [z]
+    # 1st quadrant
+    while x**2+z**2 > RE**2:
+        Bx,By,Bz = dipoleEarth(x,y,z)
+        Bmag     = np.sqrt(Bx**2+By**2+Bz**2)
+        x       += dx*Bx/Bmag
+        # y       += dy*By/Bmag
+        z       += dz*Bz/Bmag
+        X        = np.append(X,x)
+        # Y        = np.append(Y,y)
+        Z        = np.append(Z,z)
+    X = np.append(X[::-1],X)
+    Z = np.append(Z[::-1],-Z)
+    Y = np.sin((MLT-12)*15./180.*np.pi)*X
+    X = np.cos((MLT-12)*15./180.*np.pi)*X
+    return X,Y,Z
+
+
+
+
+def blines(y,x, filament, current):
+    X=y[0]
+    Y=y[1]
+    Z=y[2]
+    direction=y[3]
+    point = np.array([ [X], [Y], [Z] ])
+    B     = biotsavart( filament, current, point )
+    Bnorm = np.sqrt(B[0]*B[0] + B[1]*B[1] + B[2]*B[2])
+    dY    = np.zeros(4)
+    dY[0] = direction * B[0]/Bnorm
+    dY[1] = direction * B[1]/Bnorm
+    dY[2] = direction * B[2]/Bnorm
+    dY[3] = 0.0
+    return dY
