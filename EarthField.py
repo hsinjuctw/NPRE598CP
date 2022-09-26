5acfb76927b1dfd813df1bdd3941ea9bbef4aace
Hsinju Chen <hsinjuc2@illinois.edu>
2022-09-26 03:32:24 -0500

    DEV: Add timer

--- EarthField.py
+++ EarthField.py
@@ -4,9 +4,11 @@ from matplotlib.patches import Circle
 import Library.bearth as bearth
 import Library.coord as coord
 from matplotlib import ticker#, cm
-
+import time
 
 def main():
+    # record start time
+    start = time.time()
     ##################################
     ##### EARTH'S MAGNETIC FIELD #####
     ##################################
@@ -21,76 +23,89 @@ def main():
     Y = np.linspace( -20.*RE, 20.*RE, Ny)
     Z = np.linspace( -20.*RE, 20.*RE, Nz)
     
-    #Bx,By,Bz,Bmag = bearth.getEarthDipole(X,Y,Z)
-    Bx,By,Bz,Bmag = bearth.getEarthDipoleCSC(X,Y,Z)
+    Bx,By,Bz,Bmag = bearth.getEarthDipole(X,Y,Z)
+    #Bx,By,Bz,Bmag = bearth.getEarthDipoleCSC(X,Y,Z)
 
-    # print(np.nanmax(Bmag))
-    # print(np.nanmin(Bmag[:,:,25]))
 
-    # X0  = 10.*RE
-    # Y0  = 0.
-    # Z0  = 0.
-    # X0  = .35*RE
-    # Y0  = 0.
-    # Z0  = -.94*RE
     Lshell = 10.
     MLT    = 12.
     dX  = .01*RE
-    # dY  = .01*RE
     dZ  = .01*RE
-    # rdtheta = np.pi/1800.
 
     FLx0,FLy0,FLz0 = bearth.dipoleFieldline2D(5.,12.,dX,dZ)
-    FLx1,FLy1,FLz1 = bearth.dipoleFieldline2D(10.,12.,dX,dZ)
-    FLx2,FLy2,FLz2 = bearth.dipoleFieldline2D(15.,12.,dX,dZ)
-    FLx3,FLy3,FLz3 = bearth.dipoleFieldline2D(5.,0.,dX,dZ)
-    FLx4,FLy4,FLz4 = bearth.dipoleFieldline2D(10.,0.,dX,dZ)
+    FLx1,FLy1,FLz1 = bearth.dipoleFieldline2D(5.,0.,dX,dZ)
+    FLx2,FLy2,FLz2 = bearth.dipoleFieldline2D(10.,12.,dX,dZ)
+    FLx3,FLy3,FLz3 = bearth.dipoleFieldline2D(10.,0.,dX,dZ)
+    FLx4,FLy4,FLz4 = bearth.dipoleFieldline2D(15.,12.,dX,dZ)
     FLx5,FLy5,FLz5 = bearth.dipoleFieldline2D(15.,0.,dX,dZ)
+    FLx6,FLy6,FLz6 = bearth.dipoleFieldline2D(20.,12.,dX,dZ)
+    FLx7,FLy7,FLz7 = bearth.dipoleFieldline2D(20.,0.,dX,dZ)
 
+    # record end time
+    end = time.time()
+    print('The time of execution of above program is :',(end-start) * 10**3, ' ms')
+
+    #################
+    ##### PLOTS #####
+    #################
     cmap = 'PRGn'
-    # cmap = 'RdBu'
-    # plt.figure(1)
+
+    plt.rc('legend',fontsize=12)
+    plt.rc('axes',labelsize=12)
+    plt.rc('xtick',labelsize=12)
+    plt.rc('ytick',labelsize=12)
+    # fig, ax1 = plt.subplots(figsize=(4.8, 4.8))
     fig, ax1 = plt.subplots()
     XX,ZZ = np.meshgrid(X,Z)
     plt.contourf(np.transpose(XX)/RE,np.transpose(ZZ)/RE, Bmag[:,50,:],locator=ticker.LogLocator(),alpha=.5,cmap=cmap)
     ax1.add_artist(Circle((0,0), 1, color='b'))
-    plt.plot( FLx0/RE,FLz0/RE, 'k-' )
-    plt.plot( FLx1/RE,FLz1/RE, 'k-' )
-    plt.plot( FLx2/RE,FLz2/RE, 'k-' )
-    plt.plot( FLx3/RE,FLz3/RE, 'k-' )
-    plt.plot( FLx4/RE,FLz4/RE, 'k-' )
-    plt.plot( FLx5/RE,FLz5/RE, 'k-' )
+    plt.plot( FLx0/RE,FLz0/RE, 'b-', linewidth=.5 )
+    plt.plot( FLx1/RE,FLz1/RE, 'b-', linewidth=.5 )
+    plt.plot( FLx2/RE,FLz2/RE, 'b-', linewidth=.5 )
+    plt.plot( FLx3/RE,FLz3/RE, 'b-', linewidth=.5 )
+    plt.plot( FLx4/RE,FLz4/RE, 'b-', linewidth=.5 )
+    plt.plot( FLx5/RE,FLz5/RE, 'b-', linewidth=.5 )
+    plt.plot( FLx6/RE,FLz6/RE, 'b-', linewidth=.5 )
+    plt.plot( FLx7/RE,FLz7/RE, 'b-', linewidth=.5 )
     plt.xlabel('$X$ [$R_E$]')
     plt.ylabel('$Z$ [$R_E$]')
-    plt.axis('equal')
-    plt.colorbar()
-    plt.title('B-field magnitude in XZ plane [T] - Earth magnetic field')
+    cbar = plt.colorbar(format='%.0e')
+    cbar.set_label('Magnetic field strength (T)', rotation=270, labelpad=15)
+    ax1.set_aspect('equal', 'box')
+    # plt.legend(loc=3)
+    plt.tight_layout()
+    # plt.title('B-field magnitude in XZ plane [T] - Earth magnetic field')
     plt.savefig('dipoleEarth_XZ.png',dpi=150)
     plt.show()
-    
-    # plt.figure(2)
-    fig, ax2 = plt.subplots()
+
+    fig, ax2 = plt.subplots(figsize=(4.8, 4.8))
     XX,YY = np.meshgrid(X,Y)
-    plt.contourf(np.transpose(XX)/RE,np.transpose(YY)/RE, Bmag[:,:,50],locator=ticker.LogLocator(),cmap=cmap)
+    plt.contourf(np.transpose(XX)/RE,np.transpose(YY)/RE, Bmag[:,:,50],locator=ticker.LogLocator(),alpha=.5,cmap=cmap)
     ax2.add_artist(Circle((0,0), 1, color='b'))
     plt.xlabel('$X$ [$R_E$]')
     plt.ylabel('$Y$ [$R_E$]')
-    plt.axis('equal')
-    plt.colorbar()
-    plt.title('B-field magnitude in XY plane [T] - Earth magnetic field')
+    # cbar = plt.colorbar(format='%.0e')
+    # cbar.set_label('Magnetic field strength (T)', rotation=270, labelpad=15)
+    ax2.set_aspect('equal', 'box')
+    # plt.legend(loc=3)
+    plt.tight_layout()
+    # plt.title('B-field magnitude in XY plane [T] - Earth magnetic field')
     plt.savefig('dipoleEarth_XY.png',dpi=150)
     plt.show()
     
     # plt.figure(3)
     fig, ax3 = plt.subplots()
     YY,ZZ = np.meshgrid(Y,Z)
-    plt.contourf(np.transpose(YY)/RE,np.transpose(ZZ)/RE, Bmag[50,:,:],locator=ticker.LogLocator(),cmap=cmap)
+    plt.contourf(np.transpose(YY)/RE,np.transpose(ZZ)/RE, Bmag[50,:,:],locator=ticker.LogLocator(),alpha=.5,cmap=cmap)
     ax3.add_artist(Circle((0,0), 1, color='b'))
     plt.xlabel('$Y$ [$R_E$]')
     plt.ylabel('$Z$ [$R_E$]')
-    plt.axis('equal')
-    plt.colorbar()
-    plt.title('B-field magnitude in YZ plane [T] - Earth magnetic field')
+    # cbar = plt.colorbar(format='%.0e')
+    # cbar.set_label('Magnetic field strength (T)', rotation=270, labelpad=15)
+    ax3.set_aspect('equal', 'box')
+    # plt.legend(loc=3)
+    plt.tight_layout()
+    # plt.title('B-field magnitude in YZ plane [T] - Earth magnetic field')
     plt.savefig('dipoleEarth_YZ.png',dpi=150)
     plt.show()
 
